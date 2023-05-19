dafbbfe71d8476219d9ca584b5e0b819c71d3c98
Tuanhv <tuanhv@nhanh.vn>
2023-05-19 08:40:43 +0700

    update

--- js/main.js
+++ js/main.js
@@ -1,107 +1,89 @@
-if($('.category_option').length){
-    $('.category_option').owlCarousel({
-        nav: true,
-        margin: 5,
-        dot: false,
-        boolean: false,
-        loop: false,
-        navText: ['<i class="fas fa-chevron-left" aria-hidden="true"></i>', '<i class="fas fa-chevron-right" aria-hidden="true"></i>'],
-        items: 3,
-    });
-}
-$('.staff_item .tick .button').click(function() {
-    $(this).toggleClass('checked');
-    $('footer').toggleClass('active');
-});
-
-
-
-
+// if($('.category_option').length){
+//   $('.category_option').owlCarousel({
+//       nav: true,
+//       margin: 5,
+//       dot: false,
+//       boolean: false,
+//       loop: false,
+//       navText: ['<i class="fas fa-chevron-left" aria-hidden="true"></i>', '<i class="fas fa-chevron-right" aria-hidden="true"></i>'],
+//       items: 3,
+//   });
+// }
 
 
 $(document).ready(function() {
-    $('.category_option .category_item').bind('click', function(e) {
-      e.preventDefault(); // Ngăn chặn hành vi mặc định của click
-  
-      var target = $(this).attr("data-href"); // Lấy đích cuộn từ thuộc tính data-href của danh mục
-  
-      // Thực hiện cuộn mượt bằng cách lấy vị trí top của phần tử đích và đặt nó làm mục tiêu cuộn
-      $('html, body').stop().animate({
-        scrollTop: $(target).offset().top
-      }, 600, function() {
-        location.hash = target; // Gắn kết hash (#jumptarget) vào địa chỉ trang
-      });
-  
-      // Gỡ bỏ lớp active khỏi tất cả các mục danh mục
-      $('.category_item').removeClass('active');
-      // Gán lớp active cho mục danh mục được click
-      $(this).addClass('active');
-    });
-  });
-
-// $(window).scroll(function() {
-//     var scrollDistance = $(window).scrollTop();
-
-//     // Show/hide menu on scroll
-//     if (scrollDistance >= 850) {
-//     		$('nav').fadeIn("fast");
-//     } else {
-//     		$('nav').fadeOut("fast");
-//     }
-
-//     // Assign active class to nav links while scolling
-//     $('.staff_option').each(function(i) {
-//             if ($(this).position().top <= scrollDistance) {
-//                     $('.category_item.active').removeClass('active');
-//                     $('.category_item').eq(i).addClass('active');
-//             }
-//     });
-// }).scroll();
-
-
-    // $('.category_option .category_item[data-href*=#]').bind('click', function(e) {
-    //     alert('hihi');
-    //   e.preventDefault(); // Ngăn chặn hành vi mặc định của click
-  
-    //   var target = $(this).attr("data-href"); // Lấy đích cuộn từ thuộc tính data-href của danh mục
-  
-    //   // Thực hiện cuộn mượt bằng cách lấy vị trí top của phần tử đích và đặt nó làm mục tiêu cuộn
-    //   $('html, body').stop().animate({
-    //     scrollTop: $(target).offset().top
-    //   }, 600, function() {
-    //     location.hash = target; // Gắn kết hash (#jumptarget) vào địa chỉ trang
-    //   });
-    // });
-
-
-    var isScrolling = false;
-$(window).scroll(function() {
-  if (!isScrolling) {
-    isScrolling = true;
-    requestAnimationFrame(function() {
-      var scrollDistance = $(window).scrollTop();
-
-      if (scrollDistance >= 850) {
-        $('nav').fadeIn("fast");
-      } else {
-        $('nav').fadeOut("fast");
-      }
-
-      var prevActiveIndex = $('.category_item.active').index();
-      var targetIndex = -1;
-
-      $('.staff_option').each(function(i) {
-        if ($(this).position().top <= scrollDistance) {
-          targetIndex = i;
-        }
-      });
-
-      if (targetIndex !== prevActiveIndex) {
-        $('.category_item.active').removeClass('active');
-        $('.category_item').eq(targetIndex).addClass('active');
-      }
-
-      isScrolling = false;
+  if ($('.category_option').length) {
+    var owl = $('.category_option').owlCarousel({
+      nav: true,
+      margin: 5,
+      dots: false,
+      loop: false,
+      navText: ['<i class="fas fa-chevron-left" aria-hidden="true"></i>', '<i class="fas fa-chevron-right" aria-hidden="true"></i>'],
+      items: 3
     });
   }
-}).scroll();
+
+  $('.staff_item .tick .button').click(function() {
+    $(this).toggleClass('checked');
+    $('footer').toggleClass('active');
+  });
+
+  function autoSlideToActiveCategory() {
+    var activeCategoryIndex = $('.category_item.active').index();
+    owl.trigger('to.owl.carousel', [activeCategoryIndex, 300, true]);
+  }
+
+  // Thêm đoạn mã sau vào cuối document.ready để tự động lướt slide khi có danh mục có class active
+  if ($('.category_item.active').length) {
+    autoSlideToActiveCategory();
+  }
+
+  $('.category_option .category_item').bind('click', function(e) {
+    e.preventDefault();
+
+    var target = $(this).attr("data-href");
+    $('html, body').stop().animate({
+      scrollTop: $(target).offset().top
+    }, 600, function() {
+      location.hash = target;
+    });
+
+    $('.category_item').removeClass('active');
+    $(this).addClass('active');
+
+    autoSlideToActiveCategory();
+  });
+
+  var isScrolling = false;
+  $(window).scroll(function() {
+    if (!isScrolling) {
+      isScrolling = true;
+      requestAnimationFrame(function() {
+        var scrollDistance = $(window).scrollTop();
+
+        if (scrollDistance >= 350) {
+          $('nav').fadeIn("fast");
+        } else {
+          $('nav').fadeOut("fast");
+        }
+
+        var prevActiveIndex = $('.category_item.active').index();
+        var targetIndex = -1;
+
+        $('.staff_option').each(function(i) {
+          if ($(this).position().top <= scrollDistance) {
+            targetIndex = i;
+          }
+        });
+
+        if (targetIndex !== prevActiveIndex) {
+          $('.category_item.active').removeClass('active');
+          $('.category_item').eq(targetIndex).addClass('active');
+          autoSlideToActiveCategory();
+        }
+
+        isScrolling = false;
+      });
+    }
+  }).scroll();
+});
